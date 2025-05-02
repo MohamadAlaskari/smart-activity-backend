f6d3d5af5bf9f63965556b2e6dfa677c7fd1a3f4
MohamadAlaskari <m_alskari96@hotmail.com>
2025-05-02 18:35:14 +0200

    Integrated Swagger documentation with global setup and custom config file

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+/**
+ * Setup Swagger Documentation
+ * @param app NestJS Application instance
+ */
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Smart Activity Backend')
+    .setDescription('API Documentation for the Smart Activity App')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // *Globale Middleware*
+  //Auto-Validation
+  // app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+  // Swagger API-Dokumentation einrichten
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
