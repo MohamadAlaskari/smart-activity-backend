303fa88858fa1117a5a4823658cdfd62be093981
MohamadAlaskari <m_alskari96@hotmail.com>
2025-05-06 20:32:40 +0200

    test for redeploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { HttpModule } from '@nestjs/axios'; /*
+import { HttpModule } from '@nestjs/axios';
 import { AiModule } from './core/ai/ai.module';
 import { WeatherModule } from './modules/weather/weather.module';
 import { EventsModule } from './modules/events/events.module';
 import { DirectionsModule } from './modules/transport/directions.module';
 import { UsersModule } from './modules/users/users.module';
-*/
+
 import { DatabaseModule } from './database/database.module';
 
 @Module({
@@ -18,12 +18,12 @@ import { DatabaseModule } from './database/database.module';
       isGlobal: true,
     }),
     DatabaseModule,
-    /*
-    AiModule,
     UsersModule,
+
+    AiModule,
     WeatherModule,
     EventsModule,
-    DirectionsModule,*/
+    DirectionsModule,
   ],
   controllers: [AppController],
   providers: [],
